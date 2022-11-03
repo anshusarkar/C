0c938979c6a77d95d09fb80367b33d3ba55e1fa1
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-03 21:01:38 +0530

    Echo

--- Java.c
+++ Java.c
@@ -10,7 +10,7 @@
     static void Alpha() {
       SystemDotoutDotprintln ("Hello world !\n");
      }
-/*  public static void main(String[] args) {                                                                                               */        
+/*  public static void main(String[] args) {                                                                                                  */        
     Alpha();
-/*   }                                                                                                                                    */
+/*   }                                                                                                                                        */
  }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,30 +1,8 @@
-if(choice>4)
-    {   
-        printf("Wrong choice...\n\n");
-    }
-    if (choice==1)
-      {
-        insert();
-      }
-    else if (choice==2)
-      {
-        if (arr[0]==0)
-          {
-            printf("Array is empty!\n");
-            printf("Array is in uendeflow");
-          }
-        else
-            {
-             display();
-             printf("]\n\n");
-            }
-        
-      }  
-    else if (choice ==3)
-      {
-        printf("Perfroming deletation\n\n");
-        delete();
-        printf("Done...\n\n");
-      }
-    }
-     printf("\n\nExiting.....\n\nEnter choice again.....\n\n");
+#include<stdio.h>
+struct hello
+{
+   int main ()
+   {
+    printf("Hello, world");
+   }
+};
