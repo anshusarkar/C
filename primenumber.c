c5002c65f407bf45bc4043596a595718894dde9d
Anshu <anshusarkar11@gmail.com>
2024-01-10 20:04:11 +0530

    echo

--- primenumber.c
+++ primenumber.c
@@ -1,35 +1,27 @@
-#include<stdio.h>
-int main ()
-{
-    int i , num , flag ;
-    printf("Enter a value here : ");
-    scanf("%d",&i);
-    
-    if((i==0||i==1))
-    {
-        printf("%d is not a prime number !",i);
+#include <stdio.h>
+
+int main() {
+    int i, num, flag = 0;
+
+    printf("Enter a value here: ");
+    scanf("%d", &num);
+
+    if (num == 0 || num == 1) {
+        printf("%d is not a prime number!\n", num);
+    } else {
+        for (int j = 2; j <= num / 2; ++j) {
+            if (num % j == 0) {
+                flag = 1;
+                break;  // exit the loop if a factor is found
+            }
+        }
+
+        if (flag == 0) {
+            printf("%d is a prime number!\n", num);
+        } else {
+            printf("%d is not a prime number!\n", num);
+        }
     }
 
-
-    for (int j = 2; j <= i / 2; ++j)
-    {
-      if (num % j == 0)
-       {
-         flag = 1 ;
-       }
-       else
-       {
-        flag = 0 ;
-       }
-    }
-
-    if (flag == 1)
-    {
-        printf("%d is a prime number !",i);
-    }
-    else
-    {
-        printf("%d is not a prime numner !",i);
-    }
-
+    return 0;
 }
