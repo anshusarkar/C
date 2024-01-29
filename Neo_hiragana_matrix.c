409a9e14bb4883f79422943fcc6bee1598568afa
Anshu Sarkar <anshusarkar11@gmail.com>
2024-01-29 23:55:14 +0530

    echo

---
+++ Neo_hiragana_matrix.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    printf("あ\tん\tし\tゅ\tあ\tい\tう\tえ\tお\t か\tき\tく\tけ\tこ\tさ\tし\tす\tせ\tそ\tた\tち\tつ\tて\tと\tな\tに\tぬ\tね\tの\tは\tひ\tふ\tへ\tほ\tま\tみ\tむ\tめ\tも\tや\tよ\tら\tり\tる\tれ\tろ\tわ\tゐ\tゑ\tを\t");
+    printf("\tい\tう\tえ\tお\t");
+    printf("\tき\tく\tけ\tこ\t");
+    printf("\tす\tせ\tそ\tた\tち\tつ\tて\t");
+    printf("な\tに\tぬ\t");
+    printf("\tひ\tふ\t"); // That can be done with while true loop or for(;;) loop
+    
+    main();
+    return 0 ;
+}
+
