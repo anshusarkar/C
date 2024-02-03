563dc0625dc7ab17074ee635ac9b83590c5defe5
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-03 22:58:15 +0530

    echo

--- Delayed_Processing.c
+++ Delayed_Processing.c
@@ -1,11 +1,13 @@
+//Execute in windows for abousute fun 
 #include<stdio.h>
 #include<time.h>
 #include<unistd.h>
 void func(){
 	int i = 0 ;
-	printf("Enter a value : ");
+	printf("Enter an integer value : ");
 	scanf("%d" , &i);
-	
+	printf("\n\nSlow computer noises....");
+	sleep(3);	
 	printf("\n\numm....... ");
 	sleep(3);
 	printf("\n\nI am a slow computer");
@@ -14,9 +16,11 @@ void func(){
 
     int  j ;
     for(j = 1 ; j<20 ; j++){
-    	sleep(2);
+    	sleep(1);
     	if(j==10){
-    		printf("\n\nI will tell you what you have givven as input but wait a bit more ");
+			printf("\n\n\t\tI need more ram !! Download some ASAP !");
+			sleep(2);
+    		printf("\n\nI will tell you what you have given as input but wait a bit more ");
 		}
     	printf(".");
 
@@ -27,12 +31,15 @@ void func(){
 	
 }
 
-
 int main(){
 	float var = clock();
 	func();             
 	float var2 = clock();	
-	printf("\n\nThe execution time for the function was : %.3f sec" , (var2 - var)/1000);
+	printf("\n\nThe execution time for the function was : %.3f sec - 15 secs os slowness.... \n\n  " , (var2 - var)/1000);
+
+	//If done in windows processing tim differs and wating .. works as desired but why the dirrence even if the used compiler is same ?
 	
 	return 0;
 }
+
+
