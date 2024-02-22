a1be07d768abaeb2da45bd801c213b76fe05aa6c
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-22 21:21:49 +0530

    echo

--- Taking_String_input.c
+++ Taking_String_input.c
@@ -7,7 +7,7 @@ int main()
    printf("Enter your name here (Max 20 char limits) : ");
    
    fgets(str, MAX_LIMIT, stdin);
-   printf("%s", str);
+   printf("You entered : %s", str);
  
    return 0;
 }
