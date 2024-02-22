dc8a72100797f0be772c76466c4333aa9b61687c
Anshu Sarkar <anshusarkar11@gmail.com>
2024-02-22 21:21:02 +0530

    echo

---
+++ Taking_String_input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAX_LIMIT 20
+int main()
+{
+   char str[MAX_LIMIT];
+   
+   printf("Enter your name here (Max 20 char limits) : ");
+   
+   fgets(str, MAX_LIMIT, stdin);
+   printf("%s", str);
+ 
+   return 0;
+}
