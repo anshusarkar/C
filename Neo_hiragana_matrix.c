7d6b955a69252a7d2acb83ce1872e42862147946
Anshu Sarkar <anshusarkar11@gmail.com>
2024-03-01 21:05:22 +0530

    echo

--- Neo_hiragana_matrix.c
+++ Neo_hiragana_matrix.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 main()
 {
+    // char arr[100] = {'あ', 'い', 'う', 'え', 'お',
+    // 'か', 'き', 'く', 'け', 'こ',
+    // 'さ', 'し', 'す', 'せ', 'そ',
+    // 'た', 'ち', 'つ', 'て', 'と',
+    // 'な', 'に', 'ぬ', 'ね', 'の',
+    // 'は', 'ひ', 'ふ', 'へ', 'ほ',
+    // 'ま', 'み', 'む', 'め', 'も',
+    // 'や', 'ゆ', 'よ',
+    // 'ら', 'り', 'る', 'れ', 'ろ',
+    // 'わ', 'を', 'ん'};
     printf("あ\tん\tし\tゅ\tあ\tい\tう\tえ\tお\t か\tき\tく\tけ\tこ\tさ\tし\tす\tせ\tそ\tた\tち\tつ\tて\tと\tな\tに\tぬ\tね\tの\tは\tひ\tふ\tへ\tほ\tま\tみ\tむ\tめ\tも\tや\tよ\tら\tり\tる\tれ\tろ\tわ\tゐ\tゑ\tを\t");
     printf("\tい\tう\tえ\tお\t");
     printf("\tき\tく\tけ\tこ\t");
@@ -8,6 +19,16 @@ main()
     printf("な\tに\tぬ\t");
     printf("\tひ\tふ\t"); // That can be done with while true loop or for(;;) loop
     
+    // Claculating the the size of the array
+
+    //int size = sizeof(arr) / sizeof(char) ;
+
+    // Generating a random index 
+
+    // int random_index = rand() % 100 ;
+
+    // printf("%c", arr[random_index] );
+
     main();
     return 0 ;
 }
