69890bc6f4b32e3076fc16a08f313e12965cdcc2
anshusarkar <anshusarkar11@gmail.com>
2024-03-19 23:06:20 +0530

    echo

--- Vowel_consonent_determiner.c
+++ Vowel_consonent_determiner.c
@@ -3,12 +3,12 @@
 int main()
 {
  char ch;
- printf("Enter a character here :");
+ printf("Enter a character here : ");
  scanf("%c" , &ch );
  if (ch == 'a' || ch =='e' || ch == 'i' || ch == 'o' || ch == 'u'|| ch == 'A' || ch =='E' || ch == 'I' || ch == 'O' || ch == 'U')
- printf("it's a vowel");
+ printf("it's a vowel\n");
  else
- printf("It's a consonant");
+ printf("It's a consonant\n");
 
 
     return 0;
