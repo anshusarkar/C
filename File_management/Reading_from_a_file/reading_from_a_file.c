ba444bdf9e116c7ced35caf99f67ae8a514835db
anshusarkar <anshusarkar11@gmail.com>
2024-04-07 11:16:29 +0530

    echo

--- File_management/Reading_from_a_file/reading_from_a_file.c
+++ File_management/Reading_from_a_file/reading_from_a_file.c
@@ -4,8 +4,8 @@ int main () {
 
 	FILE* file;
 	char str[100];
-	file = fopen("/home/anshu/Programing/C/File_management/Writing_in_a_file/File_with_a_string_in_it.txt" , "r");
-	printf("The Strings/strings written into the 'File_with_a_string_in_it.txt' is : ");
+	file = fopen("/home/anshu/Programing/C/File_management/Writing_in_a_file/File_with_a_string_in_it.txt" , "r"); // That's an absolute path so don't run on windows 
+	printf("The Strings/strings written into the 'File_with_a_string_in_it.txt' is : ");						
 	
 	while(fgets(str, 100, file)){
 		printf("%s", str);
