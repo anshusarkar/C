b5d9054f0206d871387fe9aec313d48343fb2da7
anshusarkar <anshusarkar11@gmail.com>
2024-04-07 11:15:09 +0530

    echo

--- Macros.c
+++ Macros.c
@@ -3,13 +3,13 @@
 #define m main // That's a macro of main function :)
 int m () {
     
-#if (NUMBER>0)  
+#if (NUMBER>0)  // These are called a conditional directive 
 printf("Value of Number greater than 0 is: %d",NUMBER);
+#elif (NUMBER>1) 
+printf("Value of Number less than 1 is: %d",NUMBER);
 #else
 printf("Value of Number less than 0 is: %d\n",NUMBER);
-
 // #endif is used to determine the end the if condition.
-#endif         
- 
+#endif    // All condiotional directives must end with #endif     
     return 0;
 }
