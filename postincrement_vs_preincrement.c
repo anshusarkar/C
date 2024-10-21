cb6e9bf1dbf548d97926665a0626d72ef924abc1
Anshu Sarkar <anshusarkar11@gmail.com>
2024-10-21 07:01:15 +0000

    echo

---
+++ postincrement_vs_preincrement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+
+int i = 5;
+
+int result1 = ++i; // result1 will be 6 (i becomes 6 before being used)
+
+int result2 = i++; // result2 will be 5 (i becomes 6 after being used) 
+
+    return 0 ;
+}
